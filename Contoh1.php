fe40e0321a150346da101a466b9c64d2ce540005
Lenovo <tutiwiyarsih@gmail.com>
2021-09-27 20:50:47 +0700

    tugas pertemuan 1

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkernalkan</h1>";
+        echo"Nama saya Imam Nawawi
+            Saya tinggal di daerah Ciputat
+            Olah raga yang saya sukai adalah
+            Bulutangkis";
+    }
+}
---
+++ Latihan1.php
@@ -0,0 +1,16 @@
+<?php 
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Selamat Datang.. selamat belajar Web Programming";
+        //$this->load->view('view-latihan1');
+    }
+    public function penjumlahan($n1, $n2)
+    {
+        $this->load->model('Model-Lathian2');
+        $hasil= $this->Model_Latihan2->jumlah($n1, $n2);
+        echo "Hasil Penjumlahan dari" . $n1 . "+" . $n2 ." = "
+    .$hasil;
+    }
+}

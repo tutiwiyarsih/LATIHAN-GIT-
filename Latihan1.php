431fdbb831f84e60feeff77ccaf8b2e7cffca377
Lenovo <tutiwiyarsih@gmail.com>
2021-10-10 16:54:39 +0700

    Latihan1

--- Latihan1.php
+++ Latihan1.php
@@ -4,13 +4,12 @@ class Latihan1 extends CI_Controller
     public function index()
     {
         echo "Selamat Datang.. selamat belajar Web Programming";
-        //$this->load->view('view-latihan1');
+       //$this->load->view-Latihan1');
     }
     public function penjumlahan($n1, $n2)
     {
-        $this->load->model('Model-Lathian2');
-        $hasil= $this->Model_Latihan2->jumlah($n1, $n2);
-        echo "Hasil Penjumlahan dari" . $n1 . "+" . $n2 ." = "
-    .$hasil;
+        $this->load->models('Model_Latihan1');
+        $hasil = $this->Model_Latihan1->jumlah($n1, $n2);
+        echo "Hasil Penjumlahan dari". $n1 ."+". $n2 ."=" .$hasil;
     }
 }
